412309225e0eed5447e40904fe49aacc18aaaa6e
Josipa <Josipa@192.168.1.11>
2023-08-21 17:01:29 +0200

    customizing code for .jar file

--- src/memory_game/Memory.java
+++ src/memory_game/Memory.java
@@ -16,7 +16,6 @@ public class Memory extends JFrame  {
 	public JPanel panel=new JPanel(new GridLayout(4,6,0,0));
 	public static List<JButton> buttonList = new ArrayList<>();
 	public static Map<Integer,String> m =new HashMap<Integer,String>();;
-	public static BufferedImage memoryPicture;
 	public static ArrayList<BufferedImage> listImage= new ArrayList<BufferedImage>();
 	public String prosla="", trenutna="";
 	public static ArrayList<String> list= new ArrayList<String>(),zivotinje = new ArrayList<String>();;
@@ -33,7 +32,7 @@ public class Memory extends JFrame  {
 			}	
 			else {	 
 					frame.setEnabled(false);
-					buttonList.get(a).setIcon(new ImageIcon(listImage.get(a)));
+					buttonList.get(a).setIcon(new ImageIcon(getClass().getClassLoader().getResource(list.get(a))));
 					ActionListener listener = new ActionListener(){
 						public void actionPerformed(ActionEvent event){
 						    	frame.setEnabled(true);
@@ -61,7 +60,7 @@ public class Memory extends JFrame  {
 	public void reset() {		
 		for(int i=0;i<24;++i) {
 			if(buttonList.get(i).isEnabled()) {
-				buttonList.get(i).setIcon(new ImageIcon(memoryPicture));				
+				buttonList.get(i).setIcon(new ImageIcon(getClass().getClassLoader().getResource("memory.png")));				
 				list.set(i,"");
 			}
 			buttonList.get(i).setText("");				
@@ -70,24 +69,23 @@ public void kartica(ArrayList<BufferedImage> listImage,int a) {
 		if(zivotinje.contains(m.get(a))) {
 			list.set(a,m.get(a).toString()); 			
 		}	
-	buttonList.get(a).setIcon(new ImageIcon(listImage.get(a)));
+	buttonList.get(a).setIcon(new ImageIcon(getClass().getClassLoader().getResource(list.get(a))));
 	check(list.get(a),listImage,a);		
 }
 public void osnovno() throws IOException {	
 	frame.setContentPane(panel);
 	frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 	
-	frame.setVisible(true);	
-	 memoryPicture=ImageIO.read(new File("images/memory.png"));		 
+	frame.setVisible(true);		 
      int max = 23,min=0,range = max - min + 1, rand = (int)(Math.random() * range) + min;
 	 for(int i=0;i<24;++i) {		 
 		 buttonList.add(new JButton());
 		 list.add("");
 		 panel.add(buttonList.get(i));
-		 buttonList.get(i).setIcon(new ImageIcon(ImageIO.read(new File("images/memory.png"))));
+		 buttonList.get(i).setIcon(new ImageIcon(getClass().getClassLoader().getResource("memory.png")));
 		 buttonList.get(i).setText(""); 
 		 if(i<2) {
-			zivotinje.add("images/panda.jpg");zivotinje.add("images/lion.jpg");zivotinje.add("images/tiger.png");zivotinje.add("images/bird.png");zivotinje.add("images/elephant.png");zivotinje.add("images/cow.png");	
-		    zivotinje.add("images/fish.jpg");zivotinje.add("images/bambi.png");zivotinje.add("images/bee.png");zivotinje.add("images/donkey.png");zivotinje.add("images/cat.png");zivotinje.add("images/dog2.png");  		     	    	
+			    zivotinje.add("panda.jpg");zivotinje.add("lion.jpg");zivotinje.add("tiger.png");zivotinje.add("bird.png");zivotinje.add("elephant.png");zivotinje.add("cow.png");	
+			    zivotinje.add("fish.jpg");zivotinje.add("bambi.png");zivotinje.add("bee.png");zivotinje.add("donkey.png");zivotinje.add("cat.png");zivotinje.add("dog2.png");  		     	    	  		     	    	
 		 } 		 
 		 while(lst.contains(rand)) {
 				rand = (int)(Math.random() * range) + min;								
@@ -95,8 +93,8 @@ public void osnovno() throws IOException {
 		lst.add(rand);					
 		m.put(lst.get(i), zivotinje.get(i));				 
 	 }   						
-		for(int i=0;i<24;++i) {		
-			listImage.add(ImageIO.read(new File(m.get(i))));	
+		for(int i=0;i<24;++i) {	
+			listImage.add(ImageIO.read(Memory.class.getResourceAsStream("/"+m.get(i))));	
 			final Integer var = new Integer(i);				
 			buttonList.get(i).addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent arg0){
